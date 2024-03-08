063c2867ea4b28c2c4a20e4fc952e1aa45d37c3e
IceShuttle <IceShuttle@Debian>
2024-03-08 11:29:16 +0530

    model_and_sound_added

--- drone.gd
+++ drone.gd
@@ -1,9 +1,11 @@
 extends RigidBody3D
 
-var altitude = 10
+@export var altitude = 20
 var power_factor = 200
 var max_power = 12
 var total_err = 0
+var last_err = 0
+var force = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,8 +20,9 @@ func _process(_delta):
 func _physics_process(delta):
 	var err = altitude-global_position.y
 	total_err+=err
-	print(total_err*.001)
-	var force = (total_err*.001+err)*delta*power_factor
+	var derivative = err-last_err
+	force = (total_err*.001+err+derivative*10)*delta*power_factor
 	force = clamp(force,0,max_power)*Vector3.UP
 	apply_central_force(force)
+	last_err = err
 
---
+++ fan.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@export var Drone = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_rotate(Vector3.UP,Drone.force.length())
