0a22ed590ccd5117dea1d07ebd98544390379a84
IceShuttle <IceShuttle@Debian>
2024-03-08 12:36:31 +0530

    refactoring

--- drone.gd
+++ drone.gd
@@ -1,5 +1,9 @@
 extends RigidBody3D
 
+const P_GAIN = 1
+const I_GAIN = 0.001
+const D_GAIN = 10
+
 @export var altitude = 20
 var power_factor = 200
 var max_power = 12
@@ -21,7 +25,7 @@ func _physics_process(delta):
 	var err = altitude-global_position.y
 	total_err+=err
 	var derivative = err-last_err
-	force = (total_err*.001+err+derivative*10)*delta*power_factor
+	force = (err*P_GAIN+total_err*I_GAIN+derivative*D_GAIN)*delta*power_factor
 	force = clamp(force,0,max_power)*Vector3.UP
 	apply_central_force(force)
 	last_err = err
